56dacb8af3f13d2bf04e8e0d38578eee0529d2df
shoblik <shoblik@yahoo.com>
2023-03-15 17:18:36 -0700

    Creating a custom hook so we can reuse the getUsers functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,9 @@
-import { AxiosResponse } from 'axios';
-import { useEffect, useState } from 'react';
-import { CanceledError } from './services/api-client';
+import useUsers from './hooks/useUsers';
 import userService, { User } from './services/user-service';
 
 export const App = () => {
-	const [users, setUsers] = useState<User[]>([]);
-	const [error, setError] = useState('');
-	const [isLoading, setLoading] = useState(false);
 
-	useEffect(() => {
-		setLoading(true);
-		const { request, cancel } = userService.getAll<User>();
-		request
-			.then((res) => {
-				setUsers(res.data);
-				setError('');
-				setLoading(false);
-			})
-			.catch((error) => {
-				if (error instanceof CanceledError) return;
-				setError(error.message);
-				setLoading(false);
-			});
-
-		return () => cancel();
-	}, []);
+	const {users, error, isLoading, setUsers, setError} = useUsers();
 
 	const deleteUser = (user: User) => {
 		const originalUsers = [...users];
